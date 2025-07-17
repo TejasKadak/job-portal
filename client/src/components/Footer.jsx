25f737da0298a3f80074ba0cb0d1a2060c310f80
tejaskadak <tejas.02001@gmail.com>
2025-07-17 18:45:43 +0530

    frontend completed

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import { assets } from '../assets/assets'
-
+import React from "react";
+import { assets } from "../assets/assets";
 
 const Footer = () => {
   return (
-    <div className='container px-4 2xl:px-20 mx-auto flex items-center justify-between gap-4 py-3 mt-20 mb-1'>
-      <img className='h-10 w-auto max-w-full rounded-xl object-contain' src={assets.logo} alt="" />
-<p className="flex-1 pl-4 py-2 text-sm text-gray-600 hidden sm:block">
- © tejas.dev | All rights reserved
-</p>
+    <div className="container px-4 2xl:px-10 mx-auto flex items-center justify-between gap-4 py-3 mt-25 ">
+      <img
+        className="h-10 w-auto max-w-full rounded-xl object-contain"
+        src={assets.logo}
+        alt=""
+      />
+      <p className="flex-1 pl-4 py-2 text-sm text-gray-600 hidden sm:block">
+        © tejas.dev | All rights reserved
+      </p>
 
-      <div className='flex gap-2'>
+      <div className="flex gap-2">
         <img width={38} src={assets.facebook_icon} alt="" />
         <img width={38} src={assets.twitter_icon} alt="" />
         <img width={38} src={assets.instagram_icon} alt="" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
